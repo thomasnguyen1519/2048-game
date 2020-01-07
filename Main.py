3bb999c333104ec0aa53813233b5930b9bc003d2
Thomas Nguyen <ngutt96@uw.edu>
2020-01-06 22:56:39 -0800

    Completed driver code

--- Main.py
+++ Main.py
@@ -1,4 +1,43 @@
 
-
+import Board
 
 def main():
+    intro()
+    explain_controls()
+    play_game()
+
+def intro():
+    print('This is a program that plays the 2048 game.')
+    print('You combine like-numbered blocks on the game board')
+    print('and the objective is to make the illusive 2048 block.')
+    print('It currently runs in the console, but don\'t worry')
+    print('it will be ported over to a GUI soon :).')
+    print('Let\'s start playing!!')
+    print()
+
+def explain_controls():
+    print('Use the W-A-S-D keys to cascade the blocks in a certain')
+    print('direction. W cascades upwards. A cascades left.')
+    print('S cascades downwards. D cascades right.')
+    print()
+    print('Goodluck!')
+    print()
+
+def play_game():
+    board = Board.GameBoard()
+    while not board.is_game_over():
+        print('Score:', board.score(), end = '\n\n')
+        print(board, end = '\n\n')
+        print('What move would you like to make (', 'w, ', 'a, ', 's, ', 'd)? ', sep = "", end = "")
+        move = str(input())
+        while move != 'w' and move != 'a' and move != 's' and move != 'd':
+            print('Invalid move.')
+            print('What move would you like to make (', 'w, ', 'a, ', 's, ', 'd)? ', sep = "", end = "")
+            move = str(input())
+        print()
+        board.move(move)
+    print('Game over!!', end = '\n\n')
+    print('Your score was', board.score())
+
+if __name__ == '__main__':
+    main()
