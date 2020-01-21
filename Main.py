50374cc9da205d7e148f78d1849f9adad49fc99a
Thomas Nguyen <ngutt96@uw.edu>
2020-01-21 05:49:56 -0800

    added loading and saving of hiscore data

--- Main.py
+++ Main.py
@@ -1,14 +1,18 @@
 
 import Board
 import json
+import sys
+
+# serialize and deserialize a board
 
 def main():
     intro()
     explain_controls()
-    play_game()
+    data = load_data('data.json')
+    play_game(data)
 
 def intro():
-    print('\nThis is a program that plays the 2048 game.')
+    print('\nThis is a program runs the classic 2048 game.')
     print('You combine like-numbered blocks on the game board,')
     print('and the objective is to make the illusive 2048 block.')
     print('Try to accumulate the highest score if that is too easy.')
@@ -22,23 +26,48 @@ def explain_controls():
     print('S cascades downwards. D cascades right.', end = '\n\n')
     print('Goodluck!', end = '\n\n')
 
-def play_game():
+def main_menu():
+    pass
+
+def load_data(rel_path):
+    try:
+        data_file = open(rel_path)
+    except FileNotFoundError:
+        print(rel_path, ' file was not found.')
+    except OSError as err:
+        print("OS error: {0}".format(err))
+    except:
+        print("An unexpected error occured when loading the file.")
+        print("Shutting down...")
+        sys.exit(1)
+    else:
+        data = json.load(data_file)
+        return data["hiscore"], data["largest_block"]
+
+def play_game(hiscores):
     board = Board.GameBoard()
+    print('All Time Hiscore:', hiscores[0])
+    print('Largest block built:', hiscores[1])
     while not board.is_game_over():
-        print('Score:', board.score(), end = '\n\n')
+        print('\nScore:', board.score(), end = '\n\n')
         print(board, end = '\n\n')
-        print('What move would you like to make (', 'w, ', 'a, ', 's, ', 'd)? ', sep = "", end = "")
+        print('What move would you like to make? Up (w), Left (a), Down (s), Right (d)? ', sep = "", end = "")
         move = str(input())
         while move != 'w' and move != 'a' and move != 's' and move != 'd':
             print('Invalid move.', end = '\n\n')
             print('What move would you like to make (', 'w, ', 'a, ', 's, ', 'd)? ', sep = "", end = "")
             move = str(input())
-        print()
         board.move(move)
     print(board, end = '\n\n')
     print('Game over!!', end = '\n\n')
-    print('Your score was ' + str(board.score()) + '!', end = '\n\n')
-    print('Saving data...')
+    print('Your score was ' + str(board.score()) + '!', end = "\n\n")
+    print('Saving your score...')
+    if hiscores[0] <= board.score() or hiscores[1] <= board.largest_block():
+        data = {"hiscore": max(hiscores[0], board.score()), "largest_block": max(hiscores[1], board.largest_block())}
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile, skipkeys = True, indent = 4)
+    print('Done.\n')
+
 
 if __name__ == '__main__':
     main()
